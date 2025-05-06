93a460b770628e6511b331a76a809ece13e953e5
Serhat <s.ciftci561@gmail.com>
2025-05-07 01:53:56 +0300

    VeriTabanı Bağlantısı RentAcar.Persistence katmanına Yapıldı. Context altında

---
+++ Infrastructure/RentAcar.Persistence/Context/RentCarDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentAcar.Persistence.Context
+{
+    public class RentCarDbContext :DbContext
+    {
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SERHAT\\SQLEXPRESS; Database=RentedCarDb; Trusted_Connection=True; TrustServerCertificate=True;");
+        }
+        public DbSet<RentedCar> RentedCars { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
