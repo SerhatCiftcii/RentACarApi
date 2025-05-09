71ae393fa9e9effbf5fda93833c812e11399f8a4
Serhat <s.ciftci561@gmail.com>
2025-05-09 03:12:00 +0300

    Rented Car Repository

---
+++ Infrastructure/RentAcar.Persistence/Repositories/RentedCarRepositories/IRentedCarRepository.cs
@@ -0,0 +1,19 @@
+﻿using RentACar.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentAcar.Persistence.Repositories.RentedCarRepositories
+{
+    public   interface IRentedCarRepository
+    {
+        Task<List<RentedCar>> GetAllRentedCarAsync();
+        Task<RentedCar> GetByIdRentedCarAsync(int id); //
+        Task CreateRentedCarAsync(RentedCar model);
+        Task UpdateRentedCarsync(RentedCar model);
+        Task DeleteRentedCarAsync(RentedCar model); //service tarafında id ile yaptım.
+    }
+}
+
---
+++ Infrastructure/RentAcar.Persistence/Repositories/RentedCarRepositories/RentedCarRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentAcar.Persistence.Context;
+using RentACar.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentAcar.Persistence.Repositories.RentedCarRepositories
+{
+    public class RentedCarRepository : IRentedCarRepository
+    {
+        private readonly RentCarDbContext _context;
+
+        public RentedCarRepository(RentCarDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateRentedCarAsync(RentedCar model)
+        {
+           await _context.RentedCars.AddAsync(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteRentedCarAsync(RentedCar model)
+        {
+           _context.RentedCars.Remove(model);
+            return _context.SaveChangesAsync();
+        }
+
+        public async Task<List<RentedCar>> GetAllRentedCarAsync()
+        {
+            var value = await _context.RentedCars.ToListAsync();
+            return value;
+        }
+
+        public async Task<RentedCar> GetByIdRentedCarAsync(int id)
+        {
+           var value= await _context.RentedCars.FindAsync(id);
+            return value;
+        }
+
+        public async Task UpdateRentedCarsync(RentedCar model)
+        {
+            _context.RentedCars.Update(model);
+           await _context.SaveChangesAsync();
+        }
+    }
+}
